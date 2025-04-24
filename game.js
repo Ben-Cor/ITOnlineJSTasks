ccb15d560979c9facf21d17de21c20025d309e2a
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-24 16:20:11 +0100

    chest and character objects formed

--- game.js
+++ game.js
@@ -1,8 +1,16 @@
 const character = {
-    name: "Hero",
     hairColor: "brown",
     eyeColor: "blue",
     clothesColor: "brown",
-    health: 100,
+    isDead: false,
     coins: 0,
+    x: 0,
+    y: 0,
+}
+
+const chest = {
+    coins: 5,
+    isOpen: false,
+    x: 10,
+    y: 5,
 }
