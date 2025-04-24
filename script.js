56987798706b2ac08f318ac4b73b49b95c66b4f7
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-24 12:41:30 +0100

    added alert when page loads

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function changeTitle () {
     title.textContent = "This is a better Title!";
     title.classList.add("text-red-500");
 }
+
+window.onload = function() {
+    alert("Welcome to the page!");
+}
