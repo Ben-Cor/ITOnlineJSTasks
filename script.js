4a30ad2ad7b68900b829a847e9abf1d69be533ff
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-24 12:54:12 +0100

    additonal button functionality

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function changeTitle () {
     var title = document.getElementById("title");
-    title.textContent = "This is a better Title!";
-    title.classList.add("text-red-500");
+    if (title.textContent !== "This is a better Title!") {
+        title.textContent = "This is a better Title!";
+        title.classList.add("text-red-500");
+    } else {
+        title.textContent = "This is a Title";
+        title.classList.remove("text-red-500");
+    }
 }
 
 window.onload = function() {
