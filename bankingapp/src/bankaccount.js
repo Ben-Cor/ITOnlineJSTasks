333ecd55e816114ddaa28367a557e0fd2055323d
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-26 17:00:01 +0100

    created back account module with class

---
+++ bankingapp/src/bankaccount.js
@@ -0,0 +1,27 @@
+export default class BankAccount {
+    constructor(accountNumber, accountHolder, balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+    
+    deposit(amount) {
+        if (amount > 0) {
+        this.balance += amount;
+        return true;
+        }
+        return false;
+    }
+    
+    withdraw(amount) {
+        if (amount > 0 && amount <= this.balance) {
+        this.balance -= amount;
+        return true;
+        }
+        return false;
+    }
+    
+    getBalance() {
+        return this.balance;
+    }
+    }
