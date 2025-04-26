0b4fafe710a4e1ec5f2479d5061ce09faca0fa5c
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-26 17:11:28 +0100

    multiple accounts created

--- bankingapp/src/index.js
+++ bankingapp/src/index.js
@@ -1,4 +1,8 @@
 import './style.css'
 import {BankAccount} from './bankaccount.js';
 
-const account = new BankAccount('123456789', 'John Doe', 1250);
+const account1 = new BankAccount('123456789', 'John Doe', 1250);
+const account2 = new BankAccount('987654321', 'Jane Smith', 2500);
+const account3 = new BankAccount('456789123', 'Alice Johnson', 5000);
+const account4 = new BankAccount('789123456', 'Bob Brown', 7500);
+const account5 = new BankAccount('321654987', 'Charlie White', 10000);
