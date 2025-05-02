789f756f598be4f65daa39bb23bc78460eacc225
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-02 16:59:24 +0100

    started writing logic code for calculator

--- basic-calc/src/main.js
+++ basic-calc/src/main.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+let buttons = document.querySelectorAll("button");
+let screenOutput = document.getElementById("calcScreen").textContent;
+
+buttons.forEach(button => {
+    button.addEventListener("click", buttonPress )    
+});
+
+// function buttonPress(screenOutput) {
+//     let buttonInput = button.textContent.value
+//     switch (buttonInput) {
+//         case (buttonInput.typeof == Number):
+//             screenOutput += buttonInput;
+//         break;
+//     }
+// }
