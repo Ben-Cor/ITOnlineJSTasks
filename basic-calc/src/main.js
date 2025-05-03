2c3f1f98b1e009306172d4ac720d493ab72634ec
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-03 10:24:47 +0100

    added functions to add text to calc screen

--- basic-calc/src/main.js
+++ basic-calc/src/main.js
@@ -1,18 +1,7 @@
 import './style.css'
 
 let buttons = document.querySelectorAll("button");
-let screenOutput = document.getElementById("calcScreen").textContent;
-
-buttons.forEach(button => {
-    // let value = button.textContent
-    // button.addEventListener("click", buttonPress(value))
-    button.addEventListener("mousedown", (button) => {
-        if (button.className = "p-4 bg-slate-200 m-1 shadow-lg rounded-md")
-        button.className.toggle("p-4 bg-slate-200 m-1 inset-shadow-sm rounded-md")
-    })    
-});
-
-
+let screen = document.getElementById("calcScreen");
 
 // function buttonPress(value) {
 //     switch (value) {
@@ -25,14 +14,18 @@ buttons.forEach(button => {
 // }
 
 
+// Styling for button press
 buttons.forEach(button => {
     button.addEventListener("mousedown", () => {
-        button.classList.remove('shadow-lg/20')
+        button.classList.remove('shadow-md/20')
         button.classList.add('inset-shadow-sm/20')
+        let input = button.textContent
+        if (input !== "=" | "CE" | "()")
+        screen.textContent += input
         });
     
     button.addEventListener("mouseup", () => {
         button.classList.remove('inset-shadow-sm/20')
-        button.classList.add('shadow-lg/20')
+        button.classList.add('shadow-md/20')
     })  
 })
