387068acc325f2c49008e6d43f16a589a6299a5e
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-03 12:30:25 +0100

    amended input logic for calc

--- basic-calc/src/main.js
+++ basic-calc/src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 let buttons = document.querySelectorAll("button");
 let screen = document.getElementById("calcScreen");
+let currentInput = ""
 
 // function buttonPress(value) {
 //     switch (value) {
@@ -19,9 +20,6 @@ buttons.forEach(button => {
     button.addEventListener("mousedown", () => {
         button.classList.remove('shadow-md/20')
         button.classList.add('inset-shadow-sm/20')
-        let input = button.textContent
-        if (input !== "=" | "CE" | "()")
-        screen.textContent += input
         });
     
     button.addEventListener("mouseup", () => {
@@ -29,3 +27,34 @@ buttons.forEach(button => {
         button.classList.add('shadow-md/20')
     })  
 })
+
+
+//Add to screen
+buttons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        let input = button.textContent
+        if (input == "Clear") {
+            currentInput == ""
+        } else if (input == "Del") {
+            currentInput = screen.textContent.slice(0, -1)
+        } else if (!isNaN(input)) {
+            currentInput += parseFloat(input)
+        } else if (button.classList.contains("operatorButton")) {
+            currentInput += input
+        }
+            
+
+    //     switch (input) {
+    //         case (input == "CE"):
+    //             screen.textContent == ""
+    //         break;
+    //         case (input == "Del") :
+    //             screen.textContent = screen.textContent.slice(0, -1)
+    //         break;
+    //         case (!isNaN(input)):
+    //             screen.textContent += input
+    //         break;
+    //     } 
+    screen.textContent = currentInput;
+    })
+});
