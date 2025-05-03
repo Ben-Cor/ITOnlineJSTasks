380096c7367db96967f303c098002f4f2957a602
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-03 18:23:03 +0100

    added basic JS logic for gallery

--- interactive-gallery/script.js
+++ interactive-gallery/script.js
@@ -1,3 +1,12 @@
 const gallery = document.getElementById("gallery");
 const submit = document.getElementById("submit");
 let url = document.getElementById("imgURL").value;
+
+
+function addImage() {
+    let image = document.createElement("img")
+    image.setAttribute("src", url);
+    gallery.appendChild(image);
+}
+
+submit.addEventListener("submit", addImage())
