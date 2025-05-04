e970535d5f4da45ded259b3dcd7c919510d3cb26
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-04 08:55:40 +0100

    amended gallery JS logic

--- interactive-gallery/script.js
+++ interactive-gallery/script.js
@@ -1,12 +1,26 @@
-const gallery = document.getElementById("gallery");
+
 const submit = document.getElementById("submit");
 let url = document.getElementById("imgURL").value;
 
 
 function addImage() {
-    let image = document.createElement("img")
-    image.setAttribute("src", url);
-    gallery.appendChild(image);
+    if (url === "") {
+        alert("Please enter a valid URL.");
+        return;
+    } else {
+        const gallery = document.getElementById("gallery");
+        const image = document.createElement("img");
+        image.classList.add("gallery-image");
+        image.setAttribute("src", url);
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.classList.add("remove-button");
+        removeButton.setAttribute("onclick", () => {
+            gallery.removeChild(image);
+        });
+        image.appendChild(removeButton);
+        gallery.appendChild(image);
+    }
 }
 
 submit.addEventListener("submit", addImage())
