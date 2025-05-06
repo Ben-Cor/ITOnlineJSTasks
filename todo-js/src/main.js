1770ce739952e599a4ed7450fd1453e6b987a705
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-06 12:55:15 +0100

    added JS for the to do app

--- todo-js/src/main.js
+++ todo-js/src/main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+const tasks = [];
+const taskList = document.getElementById("todo-list");
+
+function addTask(task) {
+  const taskText = document.getElementById("new-todo").value;
+  tasks.push(taskText);
+}
+
+function displayTasks() {
+  tasks.forEach((task, index) => {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = task;
+    taskItem.setAttribute("data-index", index);
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      displayTasks();
+    });
+    taskItem.appendChild(removeButton);
+    taskList.appendChild(taskItem);
+  });
+}
