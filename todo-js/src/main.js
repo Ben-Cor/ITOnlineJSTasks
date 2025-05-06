846dbdf248ad73ee2dcab24177e12720e56e52ae
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-06 13:12:08 +0100

    cleared list before re-rendering. Added additional css

--- todo-js/src/main.js
+++ todo-js/src/main.js
@@ -14,11 +14,14 @@ addButton.addEventListener("click", () => {
 })
 
 function displayTasks() {
+  taskList.innerHTML = ""; // Clear the list before displaying tasks
   tasks.forEach((task, index) => {
     const taskItem = document.createElement("li");
     taskItem.textContent = task;
     taskItem.setAttribute("data-index", index);
+    taskItem.classList.add("gap-2", "flex", "items-center", "justify-between", "mb-4");
     const removeButton = document.createElement("button");
+    removeButton.classList.add("bg-red-500", "text-white", "px-2", "py-1", "rounded");
     removeButton.textContent = "Remove";
     removeButton.addEventListener("click", () => {
       tasks.splice(index, 1);
